48a3fec3200521bb5db2ec8c824bd2c093cfe005
juliof9 <00081817@uca.edu.sv>
2020-05-02 18:50:18 -0600

    first commit

---
+++ src/main/java/com/uca/Tarealabo3NCapas/Tarealabo3NCapasApplication.java
@@ -0,0 +1,13 @@
+package com.uca.Tarealabo3NCapas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tarealabo3NCapasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tarealabo3NCapasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uca/Tarealabo3NCapas/Tarealabo3NCapasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uca.Tarealabo3NCapas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tarealabo3NCapasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
